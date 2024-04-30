a06e026052fde537e9893281f23d5b8dc1439ecf
matvey-koz <m.key.201015@gmail.com>
2024-04-30 19:40:26 -0400

    update

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// var title = document.querySelector("head > title");
-let startBtn = document.querySelector("body > button");
-startBtn.addEventListener("click", quizQstn(1));
-
 function quizQstn(questionNumber) {
     document.title = "Question " + questionNumber;
 }
