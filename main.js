1af77d77209959dd7e6577381bc2c1d850ce33dd
matvey-koz <m.key.201015@gmail.com>
2024-05-04 18:09:20 -0400

    added js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function quizQstn(questionNumber) {
     document.title = "Question " + questionNumber;
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let title = document.createElement("h2");
+    title.classList.add("title");
+    title.value = "Question " + questionNumber;
+    card.append.title;
 }
