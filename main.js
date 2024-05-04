9a226d17e92e582a59c8bb2ff36a1ce740f54d39
matvey-koz <m.key.201015@gmail.com>
2024-05-04 18:25:45 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function quizQstn(questionNumber) {
+    // change title
     document.title = "Question " + questionNumber;
+
+    // make question card
     let card = document.createElement("div");
     card.classList.add("card");
     let title = document.createElement("h2");
@@ -7,4 +10,12 @@ function quizQstn(questionNumber) {
     title.value = "Question " + questionNumber;
     card.append(title);
     document.body.appendChild(card);
+
+    //remove welcome text
+    let h2 = document.querySelector("body > h2");
+    let p = document.querySelector("body > p");
+    let button = document.querySelector("body > button");
+    h2.style.display = "none";
+    p.style.display = "none";
+    button.style.display = "none";
 }
