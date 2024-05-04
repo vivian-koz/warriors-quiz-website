361f87a2d0ab98d58383fc3e3cd5d3d357d86cac
matvey-koz <m.key.201015@gmail.com>
2024-05-04 18:50:54 -0400

    update

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ function quizQstn(questionNumber) {
     card.classList.add("card");
     let title = document.createElement("h2");
     title.classList.add("title");
+    let question = document.createElement("p");
     title.innerText = "Question " + questionNumber + ":";
     card.append(title);
+    card.append(question);
     document.body.appendChild(card);
 
     //remove welcome text
